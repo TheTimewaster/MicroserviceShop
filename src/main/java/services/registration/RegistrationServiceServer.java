c48eafada4d9acf60d75648bb0f5081709dadb23
Tu <hoangthtu157@gmail.com>
2016-03-07 19:00:32 +0100

    first working state
    - enabled working web server
    - data from json file are now rendered on launch

--- src/main/java/services/registration/RegistrationServiceServer.java
+++ src/main/java/services/registration/RegistrationServiceServer.java
@@ -6,9 +6,19 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class RegistrationServiceServer
+public class RegistrationServiceServer implements Runnable
 {
+	public void run()
+	{
+		launch(new String[]{""});
+	}
+	
 	public static void main(String[] args)
+	{
+		launch(args);
+	}
+	
+	private static void launch(String[] args)
 	{
 		// Tell Boot to look for registration-server.yml
 		System.setProperty("spring.config.name", "registration-server");
--- src/main/java/services/shop/web/WebServer.java
+++ src/main/java/services/shop/web/WebServer.java
@@ -3,20 +3,27 @@ package services.shop.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(useDefaultFilters=false)
-public class WebServer
+public class WebServer implements Runnable
 {
+	public void run()
+	{
+		launch(new String[]{""});
+	}
+	
 	public static void main(String[] args)
     {
-		System.getProperty("spring.config.name", "web-server");
-		SpringApplication.run(WebServer.class, args);
+		launch(args);	
     }
-
-//	@Bean
-//	public Weba
+	
+	private static void launch(String[] args)
+	{
+		System.setProperty("spring.config.name", "web-server");
+		SpringApplication.run(WebServer.class, args);
+	}
 }
+
